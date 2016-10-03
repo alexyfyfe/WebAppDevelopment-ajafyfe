a36e5b43e008c8e6c20635543694180817bb10ec
alexyfyfe <alexyfyfe@hotmail.com>
2016-10-03 15:45:59 +0100

    Added Function and person and text

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
         <p>
             <?php
                 function awardForCapture($personName,$specsOwned,$mugsOwned,$sausageRollsOwned){
-                    echo "Wanted: "+10*pow($specsOwned*$mugsOwned*$sausageRollsOwned,2)/2;
+                    echo "Wanted: $personName<br>Known to be in possession of the following items:<br>Specs $specsOwned<br>Mugs $mugsOwned<br>Sausages Rolls $sausageRollsOwned<br>Award for capture: £ (10*pow($specsOwned*$mugsOwned*$sausageRollsOwned,2)/2)";
                 }
 
                 awardForCapture("Joe Bloggs",2,2,2);
