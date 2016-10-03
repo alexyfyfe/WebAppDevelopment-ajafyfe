a4cbc91485e54923a231c058c20164b2ceda3d06
alexyfyfe <alexyfyfe@hotmail.com>
2016-10-03 15:33:57 +0100

    Added Function

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
     <body>
         <p>
             <?php
+                function awardForCapture($specsOwned,$mugsOwned,$sausageRollsOwned){
+                    echo 10*pow($specsOwned*$mugsOwned*$sausageRollsOwned,2)/2;
+                }
                 echo "Hello World";
             ?>
         </p>
