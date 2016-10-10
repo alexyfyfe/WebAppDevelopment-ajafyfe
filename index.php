67d0235c00cccf0512f23de37b031a65871f0dab
alexyfyfe <alexyfyfe@hotmail.com>
2016-10-10 14:10:11 +0100

    Added Function and person

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     </head>
     <body>
         <p>
+            <h1> This is an Index Page!</h1>
             <a href="http://webappdevelopment-ajfyfe.azurewebsites.net/adminer.php">adminer.php</a>
         </p>
     </body>
