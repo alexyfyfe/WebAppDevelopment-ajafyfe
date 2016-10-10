a970680866e9b4042be9f1e08df904f1215ee9e0
alexyfyfe <alexyfyfe@hotmail.com>
2016-10-10 15:47:45 +0100

    Database

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <body>
         
             <h1> This is an Index Page!</h1>
-            <a>href="http://webappdevelopment-ajfyfe.azurewebsites.net/adminer.php">adminer.php</a>
+            <a href="http://webappdevelopment-ajfyfe.azurewebsites.net/adminer.php">adminer.php</a>
             <li href="http://webappdevelopment-ajfyfe.azurewebsites.net/displayAllMovies.php">displayAllMovies.php</li>
             <li href="http://webappdevelopment-ajfyfe.azurewebsites.net/displayMoviesCreatedByMarvelStudios.php">displayMoviesCreatedByMarvelStudios.php</li>
             <li href="http://webappdevelopment-ajfyfe.azurewebsites.net/displayAllMoviesCreatedAfter2010.php">displayAllMoviesCreatedAfter2010.php</li>
