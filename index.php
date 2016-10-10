284052a0ff3977cd3ee027ebbb04afcece435342
alexyfyfe <alexyfyfe@hotmail.com>
2016-10-10 15:47:14 +0100

    Database

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
 
     </head>
     <body>
-        <ul>
+        
             <h1> This is an Index Page!</h1>
             <a>href="http://webappdevelopment-ajfyfe.azurewebsites.net/adminer.php">adminer.php</a>
             <li href="http://webappdevelopment-ajfyfe.azurewebsites.net/displayAllMovies.php">displayAllMovies.php</li>
             <li href="http://webappdevelopment-ajfyfe.azurewebsites.net/displayMoviesCreatedByMarvelStudios.php">displayMoviesCreatedByMarvelStudios.php</li>
             <li href="http://webappdevelopment-ajfyfe.azurewebsites.net/displayAllMoviesCreatedAfter2010.php">displayAllMoviesCreatedAfter2010.php</li>
             <li href="http://webappdevelopment-ajfyfe.azurewebsites.net/displayMoviesCreatedByMarvelStudios.php">displayMoviesCreatedByMarvelStudios.php</li>
-        </ul>
+        
     </body>
 </html>
