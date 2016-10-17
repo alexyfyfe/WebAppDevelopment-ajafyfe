17766eb93526765ad34abbfc6b734670ef99e773
alexyfyfe <alexyfyfe@hotmail.com>
2016-10-17 16:02:31 +0100

    Database

--- displaybattles.php
+++ displaybattles.php
@@ -1,7 +1,25 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 1401061
- * Date: 17/10/2016
- * Time: 15:17
- */
+<?php include("DBCONNECT.php"); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Show SuperHero</title>
+</head>
+<body>
+<h1>Superhero Battles!</h1>
+<ul>
+    <?
+    $sql_query = "SELECT * FROM superherobattles";
+    $result = $db->query($sql_query);
+    while($row = $result->fetch_array()){
+        $firstName = $row['firstName'];
+        $lastName = $row['lastName'];
+        $mainSuperPower = $row['mainSuperpower'];
+        $villan = $row['villan'];
+        echo "<li>The superhero known as {$firstName} {$lastName} recently faught {$villan} using {$mainSuperPower}</li>";
+    }
+    ?>
+</ul>
+</body>
+</html>
