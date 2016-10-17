7c15e4516e7e465fa5f32880d4941df2141cece9
alexyfyfe <alexyfyfe@hotmail.com>
2016-10-17 13:52:51 +0100

    Database

--- helloSelector.php
+++ helloSelector.php
@@ -4,8 +4,8 @@
     </head>
     <body>
         <p>Where would you like to say hello?</p>
-        <A href=“helloPrinter.php?planet=Earth>Earth</A>
-        <A href=“helloPrinter.php?planet=Mars>Mars</A>
-        <A href=“helloPrinter.php?planet=Uranus>Uranus</A>
+        <A href="helloPrinter.php?planet=Earth">Earth</A>
+        <A href="helloPrinter.php?planet=Mars">Mars</A>
+        <A href="helloPrinter.php?planet=Uranus">Uranus</A>
     </body>
 </html>
