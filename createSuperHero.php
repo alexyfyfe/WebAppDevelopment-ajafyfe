f699024d993ba8777477bf32a1416d6818c82f2b
alexyfyfe <alexyfyfe@hotmail.com>
2016-10-17 14:09:18 +0100

    Database

--- createSuperHero.php
+++ createSuperHero.php
@@ -4,11 +4,11 @@
     </head>
     <body>
         <form action="displaySuperHero.php" method="post">
-            <label>Forname</label><input type="text" name="forename">
-            <label>Surname</label><input type="text" name="surname">
-            <label>Gender</label><input type="text" name="gender">
-            <label>Main Superhero Power</label><input type="text" name="mainsuperheropower">
-            <input type="submit" value="Submit">
+            <p><label>Forname</label><input type="text" name="forename"></p>
+            <p><label>Surname</label><input type="text" name="surname"></p>
+            <p><label>Gender</label><input type="text" name="gender"></p>
+            <p><label>Main Superhero Power</label><input type="text" name="mainsuperheropower"></p>
+            <p><input type="submit" value="Submit"></p>
         </form>
     </body>
 </html>
--- displaySuperHero.php
+++ displaySuperHero.php
@@ -9,11 +9,6 @@
             $gender = $_POST["gender"];
             $mainsuperheropower = $_POST["mainsuperheropower"];
             echo "<p>Name: $forename $surname</p><p>Gender: $gender</p><p>Main SuperHero Power: $mainsuperheropower</p>";
-                 
-            
-            
-            
-            
         ?>
     </body>
 </html>
