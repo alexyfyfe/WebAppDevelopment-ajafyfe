3353993ff673839306d614c9ae218c2162e55aff
alexyfyfe <alexyfyfe@hotmail.com>
2016-10-17 14:09:50 +0100

    Database

--- createSuperHero.php
+++ createSuperHero.php
@@ -4,10 +4,10 @@
     </head>
     <body>
         <form action="displaySuperHero.php" method="post">
-            <p><label>Forname</label><input type="text" name="forename"></p>
-            <p><label>Surname</label><input type="text" name="surname"></p>
-            <p><label>Gender</label><input type="text" name="gender"></p>
-            <p><label>Main Superhero Power</label><input type="text" name="mainsuperheropower"></p>
+            <p><label>Forname: </label><input type="text" name="forename"></p>
+            <p><label>Surname: </label><input type="text" name="surname"></p>
+            <p><label>Gender: </label><input type="text" name="gender"></p>
+            <p><label>Main Superhero Power: </label><input type="text" name="mainsuperheropower"></p>
             <p><input type="submit" value="Submit"></p>
         </form>
     </body>
