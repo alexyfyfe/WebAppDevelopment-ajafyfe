1dca8c6665a52ce84c5432e0e80fda34ea6ce1bb
alexyfyfe <alexyfyfe@hotmail.com>
2016-10-17 13:40:56 +0100

    Database

---
+++ helloSelector.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <p>Where would you like to say hello?</p>
+        <form action="helloPrinter.php" method="post">
+            <input type="submit" value='Earth'>
+            <input type="submit" value='Mars'>
+            <input type="submit" value='Uranus'>
+        </form>
+    </body>
+</html>
