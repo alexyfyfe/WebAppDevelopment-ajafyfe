e63432c3629ddf5250a51b3b190e5951331e0c06
alexyfyfe <alexyfyfe@hotmail.com>
2016-10-17 14:56:04 +0100

    Database

--- insertsuperhero.php
+++ insertsuperhero.php
@@ -1,3 +1,18 @@
 <?php
-
+//include database
 include("DBCONNECT.php");
+
+$firstname = $_POST["firstName"];
+$lastname = $_POST["lastName"];
+$mainsuperpower = $_POST["mainSuperPower"];
+
+$sql = "INSERT INTO superheros (firstName, lastName, mainSuperpower) VALUES ('$firstname','$lastname','$mainsuperpower')";
+
+if(mysqli_query($db,$sql)){
+}else{
+    echo "Error: " . $sql . "<br>" . mysqli_errno($db);
+}
+
+header("location:Showsuperheros.php");
+
+?>
