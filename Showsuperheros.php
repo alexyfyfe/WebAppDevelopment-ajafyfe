b868e7348f7af907b0593a53dd72bf0e308f6bad
alexyfyfe <alexyfyfe@hotmail.com>
2016-10-17 15:06:07 +0100

    Database

--- Showsuperheros.php
+++ Showsuperheros.php
@@ -1,7 +1,26 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 1401061
- * Date: 17/10/2016
- * Time: 14:28
- */
+<?php include("DBCONNECT.php"); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Show SuperHero</title>
+    </head>
+    <body>
+        <h1>Superhero List</h1>
+        <p>The following superheros have registered on the site:</p>
+        <ul>
+            <?
+                $sql_query = "SELECT * FROM superheros";
+                $result = $db->query($sql_query);
+                while($row = $result->fetch_array()){
+                    $superheroID = $row['superheroID'];
+                    $firstName = $row['firstName'];
+                    $lastName = $row['lastName'];
+                    $mainSuperPower = $row['mainSuperpower'];
+                        echo "<li>{$superheroID} {$firstName} {$lastName} {$mainSuperPower}</li>";
+                }
+            ?>
+        </ul>
+    </body>
+</html>
