9592df05ba64cb7dbcbda365ba103631cc9d691c
alexyfyfe <alexyfyfe@hotmail.com>
2016-10-24 14:44:30 +0100

    Database

--- checkloginsession.php
+++ checkloginsession.php
@@ -3,11 +3,9 @@
 $username = $_POST["name"];
 $password = $_POST["password"];
 
-session_start();
-$_SESSION['username'] = $username;
-$_SESSION['password'] = $password;
 
 if($username == "username" && $password == "password"){
+    session_start();
     $_SESSION['type'] = 'standarduser';
 }
 
