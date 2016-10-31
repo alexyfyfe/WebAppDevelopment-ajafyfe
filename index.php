f84295c4b8909f0b13608873f868f7ba4a094e28
alexyfyfe <alexyfyfe@hotmail.com>
2016-10-31 13:42:25 +0000

    Database

--- index.php
+++ index.php
@@ -1,10 +1,47 @@
+<?
+define('INCLUDE_DIR', dirname(__FILE__) . '/inc/');
+$rules = array(
+    //
+    //main pages
+    //
+    'about' => "/about",
+    'contactus' => "/contactus",
+    'blog' => "/blog",
+    'blog_article' => "/blog/(?'blogID'[\w\-]+)",
+    //
+    //Admin Pages
+    //
+    'login' => "/login",
+    'create_article' => "/createarticle",
+    'logout' => "/logout",
+    //
+    // Home Page
+    //
+    'home' => "/"
+);
+$uri = rtrim(dirname($_SERVER["SCRIPT_NAME"]), '/');
+$uri = '/' . trim(str_replace($uri,
+        ''
+        , $_SERVER['REQUEST_URI']), '/');
+$uri = urldecode($uri);
+foreach ($rules as $action => $rule) {
+    if (preg_match('~^' . $rule . '$~i', $uri, $params)) {
+        include(INCLUDE_DIR . $action . '.php');
+        exit();
+    }
+}
+// nothing is found so handle the 404 error
+include(INCLUDE_DIR . '404.php');
+?>
+
+<!--
 <!DOCTYPE html>
 <html>
     <head>
 
     </head>
     <body>
-        
+
             <h1> This is an Index Page.</h1>
             <li><a href="http://webappdevelopment-ajfyfe.azurewebsites.net/adminer.php">adminer.php</a></li>
             <li><a href="http://webappdevelopment-ajfyfe.azurewebsites.net/displayAllMovies.php">displayAllMovies.php</a></li>
@@ -26,3 +63,4 @@
 
     </body>
 </html>
+           -->
