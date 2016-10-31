aaa9b1326d50ae8197d4d776a5bf965a55e7e732
alexyfyfe <alexyfyfe@hotmail.com>
2016-10-31 15:07:40 +0000

    Database

---
+++ phpcms/inc/insertuser.php
@@ -0,0 +1,17 @@
+<?php
+
+include("dbconnect.php");
+
+$username = $_POST["username"];
+$password = $_POST["password"];
+
+$sql = "INSERT INTO users (username, password) VALUES ('$username','$password')";
+
+
+if (mysqli_query($db, $sql)){
+} else {
+    echo "Error: " . $sql . "<br>" .mysqli_error($db);
+}
+
+header("location:phpcms.php");
+?>
--- phpcms/inc/signup.php
+++ phpcms/inc/signup.php
@@ -1,32 +1,18 @@
-<?php
-include("dbconnect.php");
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    include("scripts/header.php");
-    ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login Page</title>
+</head>
+<body>
     <main>
-        <form action="signup" method="post">
+        <form action="insertuser.php" method="post">
             <input type="text" name="username" placeholder="username"></br>
             <input type="password" name="password" placeholder="password"></br>
             <p><input type="submit" value="Submit"></p>
         </form>
     </main>
-
-    <?
-    include("scripts/footer.php");
-    include("scripts/dbconnect.php");
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-
-    $sql = "INSERT INTO users (username, password) VALUES ('$username','$password')";
-
-    if(mysqli_query($db,$sql)){
-    }else{
-        echo "Error: " . $sql . "<br>" . mysqli_errno($db);
-    }
-
-    header("location:phpcms.php");
-
-?>
+</body>
 
 
 
