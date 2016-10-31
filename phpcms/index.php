774c44743bb3d122a09cc5e9a60178e332fae4f3
alexyfyfe <alexyfyfe@hotmail.com>
2016-10-31 14:23:13 +0000

    Database

--- phpcms/index.php
+++ phpcms/index.php
@@ -35,34 +35,3 @@ foreach ($rules as $action => $rule) {
 // nothing is found so handle the 404 error
 include(INCLUDE_DIR . '404.php');
 ?>
-
-<!--
-<!DOCTYPE html>
-<html>
-    <head>
-
-    </head>
-    <body>
-
-            <h1> This is an Index Page.</h1>
-            <li><a href="http://webappdevelopment-ajfyfe.azurewebsites.net/adminer.php">adminer.php</a></li>
-            <li><a href="http://webappdevelopment-ajfyfe.azurewebsites.net/displayAllMovies.php">displayAllMovies.php</a></li>
-            <li><a href="http://webappdevelopment-ajfyfe.azurewebsites.net/displayMoviesCreatedByMarvelStudios.php">displayMoviesCreatedByMarvelStudios.php</a></li>
-            <li><a href="http://webappdevelopment-ajfyfe.azurewebsites.net/displayAllMoviesCreatedAfter2010.php">displayAllMoviesCreatedAfter2010.php</a></li>
-            <li><a href="http://webappdevelopment-ajfyfe.azurewebsites.net/displayAllXMenFilms.php">displayAllXMenFilms.php</a></li>
-            <li><a href="http://webappdevelopment-ajfyfe.azurewebsites.net/helloSelector.php">helloSelector.php</a></li>
-            <li><a href="http://webappdevelopment-ajfyfe.azurewebsites.net/createSuperHero.php">createSuperHero.php</a></li>
-            <li><a href="http://webappdevelopment-ajfyfe.azurewebsites.net/superheroform.html">superheroform.html</a></li>
-            <li><a href="http://webappdevelopment-ajfyfe.azurewebsites.net/Showsuperheros.php">Showsuperheros.php</a></li>
-            <li><a href="http://webappdevelopment-ajfyfe.azurewebsites.net/battle.php">battle.php</a></li>
-            <li><a href="http://webappdevelopment-ajfyfe.azurewebsites.net/displaybattles.php">displaybattles.php</a></li>
-            <br>
-            <li><a href="http://webappdevelopment-ajfyfe.azurewebsites.net/cookieIndex.html">cookieIndex.html</a></li>
-            <li><a href="http://webappdevelopment-ajfyfe.azurewebsites.net/cookieIndexSession.html">cookieIndexSession.html</a></li>
-            <br>
-            <li><a href="http://webappdevelopment-ajfyfe.azurewebsites.net/HEADER.php">HEADER.php</a></li>
-
-
-    </body>
-</html>
-           -->
